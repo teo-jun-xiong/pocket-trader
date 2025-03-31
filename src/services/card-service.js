39ee02ac9bf555be30eadb7f47fdd59aba7f9e4d
Teo Jun Xiong <teojunxiong97@gmail.com>
2025-03-31 20:29:15 +0800

    test: add test for card-services.js

--- src/services/card-service.js
+++ src/services/card-service.js
@@ -1,4 +1,5 @@
 import CardSet from './dto/card-set.js';
+import Collection from './dto/collection.js';
 
 export function extractTradableCardsForCollections(
   userCollection,
@@ -7,7 +8,7 @@ export function extractTradableCardsForCollections(
 ) {
   const tradableCollections = [];
 
-  allCollections.array.forEach((collection) => {
+  allCollections.forEach((collection) => {
     tradableCollections.push(
       extractTradableCards(userCollection, collection, minimumCount)
     );
@@ -16,24 +17,34 @@ export function extractTradableCardsForCollections(
   return tradableCollections;
 }
 
-export function extractTradableCards(userCollection, collection, minimumCount) {
-  const tradableCards = {};
+function extractTradableCards(userCollection, collection, minimumCount) {
+  const tradableCollection = new Collection(collection.getUsername());
 
   Object.values(CardSet).forEach((setName) => {
     const userCards = userCollection.getCardsForSet(setName);
     const cards = collection.getCardsForSet(setName);
     const tradable = getDifference(userCards, cards, minimumCount);
-    tradableCards[setName] = tradable;
+    tradableCollection.setCardsForSet(setName, tradable);
   });
 
-  return tradableCards;
+  return tradableCollection;
 }
 
 function getDifference(userCards, cards, minimumCount) {
   const tradable = {};
 
+  // if user does not have any cards in the set, all cards in the set are tradable
+  if (!cards) {
+    return tradable;
+  }
+
+  // if user does not have any cards, all cards in the set are tradable
+  if (!userCards) {
+    return cards;
+  }
+
   for (const [setNumber, card] of Object.entries(cards)) {
-    const userCard = userCards[`${setNumber}`];
+    const userCard = userCards[setNumber];
     if (!userCard && card['count'] > minimumCount) {
       tradable[setNumber] = card;
     }
